7557ad1dff5ef497695c1060641d1a9c9577e147
EtoZheVladoZ <vlad-ozad@ukr.net>
2024-06-07 20:18:12 +0300

    fix orders stack title

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -3,7 +3,10 @@ import { Stack } from "expo-router";
 export default function OrdersLayout() {
   return (
     <Stack>
-      <Stack.Screen name="list" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="list"
+        options={{ title: "Orders", headerShown: false }}
+      />
     </Stack>
   );
 }
