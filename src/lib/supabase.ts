28c68d73df1af54e056f370a7a01a939587a8aee
EtoZheVladoZ <vlad-ozad@ukr.net>
2024-06-12 18:38:35 +0300

    configurate supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,28 @@
+import "react-native-url-polyfill/auto";
+import * as SecureStore from "expo-secure-store";
+import { createClient } from "@supabase/supabase-js";
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = "https://qwslgsxrcamyhqpcocge.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3c2xnc3hyY2FteWhxcGNvY2dlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgyMDU3MTIsImV4cCI6MjAzMzc4MTcxMn0.BOOfV0KDELJcF4IhiTo50a-UOmfJncrp4qAA42t5CEE";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
