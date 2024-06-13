13f57227f07b5ab4c014a6ffddb4edda242cf0d0
EtoZheVladoZ <vlad-ozad@ukr.net>
2024-06-13 19:27:06 +0300

    add supabase typescript integration

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import "react-native-url-polyfill/auto";
 import * as SecureStore from "expo-secure-store";
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "@/database.types";
 
 const ExpoSecureStoreAdapter = {
   getItem: (key: string) => {
@@ -18,7 +19,7 @@ const supabaseUrl = "https://qwslgsxrcamyhqpcocge.supabase.co";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3c2xnc3hyY2FteWhxcGNvY2dlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgyMDU3MTIsImV4cCI6MjAzMzc4MTcxMn0.BOOfV0KDELJcF4IhiTo50a-UOmfJncrp4qAA42t5CEE";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: ExpoSecureStoreAdapter as any,
     autoRefreshToken: true,
